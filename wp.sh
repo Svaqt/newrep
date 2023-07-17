2649ec406809cae1a796ce235f2429c132e565c7
vi_user <mail@opendomain>
2023-07-17 11:06:27 +0000

    wp

---
+++ wp.sh
@@ -0,0 +1,16 @@
+#!/bin/bash 
+#sudo apt update 
+#sudo apt upgrade -y
+#sudo apt install apache2 apache2 apache2-utils php php-mysql libapache2-mod-php php-cli php-cgi php-gd mariadb-client mariadb-server -y
+#sudo mariadb -e "CREATE DATABASE WP;"
+#sudo mariadb -e "CREATE USER 'wpu'@'localhost' IDENTIFIED BY 'pass';"
+#sudo mariadb -e "GRANT ALL PRIVILEGES ON wp.* TO 'wpu'@'localhost';"
+#sudo mariadb -e "FLUSH PRIVILEGES;"
+#wget -c https://wordpress.org/latest.tar.gz
+#tar -xzvf latest.tar.gz
+#sudo cp -R wordpress/* /var/www/html
+#sudo chown -R www-data:www-data /var/www/html
+#sudo chmod -R 755 /var/www/html
+#sudo rm /var/www/html/index.html
+#sudo systemctl restart apache2
+
